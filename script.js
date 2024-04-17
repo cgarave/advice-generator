0ca4c78add46bb0e218fcb3c926b1093cc3ddc72
Rave Flores <reyvflores40@gmail.com>
2024-04-17 17:52:28 +0800

    added async/await function

--- script.js
+++ script.js
@@ -2,11 +2,30 @@ let displayAdvice = document.getElementById('text-container');
 let displayAdviceID = document.getElementById('adviceID');
 let randomBtn = document.getElementById('randomize').onclick = () => {
     document.getElementById('dice').classList.add('animate-spin')
-    console.log(document.getElementById('dice').className);
     setTimeout(() => location.reload(), 500)};
 
-fetch('https://api.adviceslip.com/advice')
-    .then(response => response.json())
-    .then(data => {displayAdvice.innerHTML = data.slip.advice
-                    displayAdviceID.innerHTML = `ADVICE # ${data.slip.id}`})
-    .catch(error => console.error(error));
+// fetch('https://api.adviceslip.com/advice')
+//     .then(response => response.json())
+//     .then(data => {displayAdvice.innerHTML = data.slip.advice
+//                     displayAdviceID.innerHTML = `ADVICE # ${data.slip.id}`})
+//     .catch(error => console.error(error));
+
+async function fetchData(){
+    
+    try {
+        const response = await fetch('https://api.adviceslip.com/advice');
+        const data = await response.json();
+        
+        if(!response.ok){
+            throw new Error("Error");
+        }
+
+        displayAdvice.innerHTML = data.slip.advice;
+        displayAdviceID.innerHTML = `ADVICE # ${data.slip.id}`
+    }
+    catch(error) {
+        console.log(error);
+    }
+
+}
+fetchData()
